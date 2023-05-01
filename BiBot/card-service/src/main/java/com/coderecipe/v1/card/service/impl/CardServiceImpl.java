a427cb49176e5c40fdefdbbd6371090e5d28f605
WOOJIN SON <swj1265@naver.com>
2023-05-01 17:59:09 +0900

    setup : 초기 셋업 참고 자료 작성 #SP2-24

---
+++ BiBot/card-service/src/main/java/com/coderecipe/v1/card/service/impl/CardServiceImpl.java
@@ -0,0 +1,29 @@
+package com.coderecipe.v1.card.service.impl;
+
+import com.coderecipe.v1.card.model.Card;
+import com.coderecipe.v1.card.dto.CardDTO;
+import com.coderecipe.v1.card.model.repository.ICardRepository;
+import com.coderecipe.v1.card.service.ICardService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Data
+@RequiredArgsConstructor
+@Slf4j
+public class CardServiceImpl implements ICardService {
+    private final ICardRepository iCardRepository;
+
+    @Override
+    public List<Long> addCard(List<CardDTO> req) {
+        return req.stream().map(e -> {
+            Card card = Card.of(e);
+            iCardRepository.save(card);
+            return card.getId();
+        }).toList();
+    }
+}
