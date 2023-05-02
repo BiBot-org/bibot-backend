aca969f2174d84baf4ed211833177138cc784cb4
Junseokee <102888442+Junseokee@users.noreply.github.com>
2023-05-02 11:51:07 +0900

    setup : WebConfig, 통합 gradle 수정 #S2P-25

--- BiBot/card-service/src/main/java/com/coderecipe/global/config/web/WebMvcConfig.java
+++ BiBot/card-service/src/main/java/com/coderecipe/global/config/web/WebMvcConfig.java
@@ -1,25 +1,19 @@
 package com.coderecipe.global.config.web;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Value("${client.local.url}")
-    private final String localUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowCredentials(true).maxAge(3600)
-                .allowedOriginPatterns(localUrl)
+                .allowedOriginPatterns("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*");
