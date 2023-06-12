cf26ad3e79f31c13d189daf5d97007bf3a6c1d1f
junseokee <weq156@naver.com>
2023-06-12 10:25:00 +0900

    feat : sh 파일 변경 #devops

--- BiBot/user-service/gcp-json-from-vault.sh
+++ BiBot/user-service/gcp-json-from-vault.sh
@@ -3,7 +3,9 @@
 export VAULT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 
 
-export GOOGLE_APPLICATION_CREDENTIALS=/home/bibot_recipe/my-credentials.json
-
+#export GOOGLE_APPLICATION_CREDENTIALS=/home/bibot_recipe/my-credentials.json
+# shellcheck disable=SC2002
+export GOOGLE_APPLICATION_CREDENTIALS=$(cat /home/bibot_recipe/my-credentials.json | jq -r '.data')
+echo "$GOOGLE_APPLICATION_CREDENTIALS" > gcp_credential.json
 # jar 실행
 java -jar user-service.jar
