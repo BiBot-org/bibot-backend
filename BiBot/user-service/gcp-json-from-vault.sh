ce86e2372342c2876d81d4e1a8f8cc449d570302
WOOJIN SON <swj1265@naver.com>
2023-06-12 09:37:01 +0900

    feat : 경비 처리 에러처리 및 메시지 파이프라인 통합 수정 #S2P-43
    Signed-off-by: WOOJIN SON <swj1265@naver.com>

--- BiBot/user-service/gcp-json-from-vault.sh
+++ BiBot/user-service/gcp-json-from-vault.sh
@@ -2,7 +2,8 @@
 
 export VAULT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 
+
 export GOOGLE_APPLICATION_CREDENTIALS=/home/bibot_recipe/my-credentials.json
 
 # jar 실행
-java -jar receipt-service.jar
+java -jar user-service.jar
